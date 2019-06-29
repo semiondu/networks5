72fb8d2424b02eafdf98180b2f96b8a9c86d7336
semiondu <51852367+semiondu@users.noreply.github.com>
2019-06-29 14:56:02 +0300

    Update tcp-cubic-test.py

--- tcp-cubic-test.py
+++ tcp-cubic-test.py
@@ -475,7 +475,7 @@ class ConsoleApp( Frame ):
 	#TODO define half of the hosts to be client
 	#TODO Get the iperf server IP using command like this example:   ip = consoles[i-count/2].node.IP()
 	for i in range (4):
-		consoles[4+i].sendCmd ('iperf3 -c 10.0.0.' + str(i)+ ' --time 60 > ' + file_path + str(i+4) + ' 2>&1')
+		consoles[4+i].sendCmd ('iperf3 -c 10.0.0.' + str(i+1)+ ' --time 60 > ' + file_path + str(i+1) + ' 2>&1')
 				       
 	    #TODO: write the iperf3 command to run in the client and redirect the results to <file_path/hostx> where x is the host number
 	    #TODO: For each client host use: consoles[i].sendCmd( '???? ' + ?? +  '> ' + file_path + str(i) + ' 2>&1')
